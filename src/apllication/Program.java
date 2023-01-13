69b8b39ac0eb290f4fcd195937084f7170bfc935
Atila jr <atilla.junior33@gmail.com>
2023-01-13 10:15:36 -0300

    Department entity class

---
+++ src/apllication/Program.java
@@ -0,0 +1,15 @@
+package apllication;
+
+import model.entities.Department;
+
+public class Program {
+
+  public static void main(String[] args) {
+    System.out.println("======================================");
+
+    Department obj = new Department(1, " Books ");
+    System.out.println(obj);
+
+    System.out.println("======================================");
+  }
+}
