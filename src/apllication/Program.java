405ad6fcebff8c860f73b072664eeabc535262dd
Atila jr <atilla.junior33@gmail.com>
2023-01-13 16:53:01 -0300

    Insert implementation

--- src/apllication/Program.java
+++ src/apllication/Program.java
@@ -1,5 +1,6 @@
 package apllication;
 
+import java.util.Date;
 import java.util.List;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -31,6 +32,18 @@ public class Program {
       System.out.println(obj);
     }
 
+    System.out.println("======================================");
+    System.out.println("===== TEST 4: Seller insert=======");
+    Seller newSeller = new Seller(
+      null,
+      "Greg",
+      "greg@gmail.com",
+      new Date(),
+      4000.0,
+      department
+    );
+    sellerDao.insert(newSeller);
+    System.out.println("Inserted! new id = " + newSeller.getId());
     System.out.println("======================================");
   }
 }
