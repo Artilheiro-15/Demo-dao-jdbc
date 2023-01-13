28424427fcfb4af77524349a8dc3fbd0d4c8b2ca
Atila jr <atilla.junior33@gmail.com>
2023-01-13 13:18:39 -0300

    findById implementation

--- src/apllication/Program.java
+++ src/apllication/Program.java
@@ -1,9 +1,7 @@
 package apllication;
 
-import java.util.Date;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -11,20 +9,19 @@ public class Program {
   public static void main(String[] args) {
     System.out.println("======================================");
 
-    Department obj = new Department(1, " Books ");
-    System.out.println(obj);
+    // Department obj = new Department(1, " Books ");
+    // System.out.println(obj);
 
-    Seller seller = new Seller(
-      21,
-      "Bob",
-      "Bob@gmail.com",
-      new Date(),
-      3000.0,
-      obj
-    );
-    System.out.println(seller);
+    // Seller seller = new Seller(21, "Bob", "Bob@gmail.com", new Date(), 3000.0, obj);
+    // System.out.println(seller);
+    // System.out.println("======================================");
 
     SellerDao sellerDao = DaoFactory.createSellerDao();
+
+    Seller seller = sellerDao.findById(3);
+
+    System.out.println(seller);
+
     System.out.println("======================================");
   }
 }
--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -7,6 +7,6 @@ public interface SellerDao {
   void insert(Seller obj);
   void update(Seller obj);
   void deleteById(Integer id);
-  Seller findId(Integer id);
+  Seller findById(Integer id);
   List<Seller> findAll();
 }
